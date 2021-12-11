11d9fdf5ba4c24623c22e3fc1ecd57ef96218614
Gabriel <Gabriel@Gabriel-PC>
2021-12-11 22:30:49 +0200

    AJUSTES NA TELA DISCIPLINA (BOTOES)

--- src/View/Create/Disciplina.java
+++ src/View/Create/Disciplina.java
@@ -79,11 +79,11 @@ public class Disciplina {
     
     public void botoes(){
         
-        JButton editarButton = new JButton("Editar dados");
-        editarButton.setBounds(480, 200, 150, 20);
+        JButton editarButton = new JButton("Editar Disciplina");
+        editarButton.setBounds(480, 200, 150, 35);
         editarButton.setFocusable(false);
-        JButton adicionarButton = new JButton("Adicionar");
-        adicionarButton.setBounds(480, 250, 150, 20);
+        JButton adicionarButton = new JButton("Adicionar Disciplina");
+        adicionarButton.setBounds(480, 250, 150, 35);
         adicionarButton.setFocusable(false);
         
         ctDisc.add(editarButton);
