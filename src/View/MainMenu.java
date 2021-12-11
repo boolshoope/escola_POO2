a5d8018dbf7eb3c05b70d84ca0cd93ba8e2c323a
Gabriel <Gabriel@Gabriel-PC>
2021-12-11 22:31:06 +0200

    AJUSTES NA TELA MAINMENU (BOTOES)

--- src/View/MainMenu.java
+++ src/View/MainMenu.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package View;
 
 import View.Create.Encarregado;
@@ -23,15 +23,15 @@ import javax.swing.*;
  * @author isacl
  */
 public class MainMenu extends JFrame implements ActionListener {
-
+    
     JPanel panWButtons, panNButtons, main;
     JButton[] lstButtons = new JButton[9];
     JLabel lblWLine, lblLogo, lblNSistGestEsc, lblUsername;
-
+    
     int wSize = 220;
     String path = "";
     Font f2 = new Font(Font.SANS_SERIF, Font.BOLD, 20);
-
+    
     public MainMenu() {
         //panels
         main = new JPanel(new BorderLayout());
@@ -41,17 +41,17 @@ public class MainMenu extends JFrame implements ActionListener {
         panNButtons = new JPanel();
         panNButtons.setBackground(new Color(62, 62, 62));
         panNButtons.setLayout(new BorderLayout());
-
+        
         //labels
         lblWLine = new JLabel();
         lblWLine.setMaximumSize(new Dimension(wSize, 10));
         lblWLine.setBackground(Color.WHITE);
         lblWLine.setOpaque(true);
-
+        
         path = System.getProperty("user.dir") + "/src/View/img/logo.jpg";
         lblLogo = new JLabel();
         DefinirIimagem(lblLogo, "logo", wSize, 90);
-
+        
         lblNSistGestEsc = new JLabel("Sistema de Gestao Escolar");
         lblNSistGestEsc.setFont(f2);
         lblNSistGestEsc.setForeground(Color.WHITE);
@@ -60,7 +60,7 @@ public class MainMenu extends JFrame implements ActionListener {
         lblUsername.setFont(f2);
         lblUsername.setForeground(Color.WHITE);
         lblUsername.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 20));
-
+        
         //buttons
         for (int i = 0; i < lstButtons.length; i++) {
             /*
@@ -73,7 +73,7 @@ public class MainMenu extends JFrame implements ActionListener {
             disciplina
             Turma
             ano academico
-             */
+            */
             switch (i) {
                 case 0:
                     lstButtons[i] = new JButton("Aluno");
@@ -114,35 +114,35 @@ public class MainMenu extends JFrame implements ActionListener {
             }
             lstButtons[i].addActionListener(this);
         }
-
+        
         for (int i = 0; i < lstButtons.length; i++) {
             panWButtons.add(lstButtons[i]);
         }
-
+        
         //add elements to panel
         panWButtons.add(lblWLine);
         for (int i = 0; i < lstButtons.length; i++) {
             panWButtons.add(lstButtons[i]);
         }
         main.add(panWButtons, BorderLayout.WEST);
-
+        
         panNButtons.add(lblLogo, BorderLayout.WEST);
         panNButtons.add(lblNSistGestEsc, BorderLayout.CENTER);
         panNButtons.add(lblUsername, BorderLayout.EAST);
-
+        
         main.add(panNButtons, BorderLayout.NORTH);
-
+        
         add(main);
         setSize(1366, 760);
         show();
-
+        
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
-
+    
     private void SetWButtons(JButton btn, String file) {
         Font f1 = new Font(Font.SANS_SERIF, Font.BOLD, 20);
         path = System.getProperty("user.dir") + "/src/View/img/" + file + ".png";
-
+        
         btn.setPreferredSize(new Dimension(wSize, 50));
         btn.setMaximumSize(new Dimension(wSize, 50));
         btn.setIcon(new ImageIcon(path));
@@ -152,7 +152,7 @@ public class MainMenu extends JFrame implements ActionListener {
         btn.setFont(f1);
         btn.setFocusable(false);
     }
-
+    
     private void DefinirIimagem(JLabel lbl, String imagem, int w, int h) {
         BufferedImage imgb = null;
         try {
@@ -160,21 +160,21 @@ public class MainMenu extends JFrame implements ActionListener {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        
         Image dimg = imgb.getScaledInstance(w, h, Image.SCALE_SMOOTH);
         lbl.setIcon(new ImageIcon(dimg));
     }
-
+    
     private void showForm(Component com) {
         BorderLayout layout = (BorderLayout) main.getLayout();
-        if (layout.getLayoutComponent(BorderLayout.CENTER) != null) 
+        if (layout.getLayoutComponent(BorderLayout.CENTER) != null)
             main.remove(layout.getLayoutComponent(BorderLayout.CENTER));
         
         main.add(com, BorderLayout.CENTER);
         main.repaint();
         main.revalidate();
     }
-
+    
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == lstButtons[0]) {
@@ -184,8 +184,23 @@ public class MainMenu extends JFrame implements ActionListener {
         if(e.getSource() == lstButtons[2]) {
             new Matricula();
         }
+        
+        
+        if(e.getSource() == lstButtons[3]){
+            new Professor();
+            
+        }
+        
         if (e.getSource() == lstButtons[4]) {
             showForm(new Encarregado());
+        }
+        
+        if(e.getSource()== lstButtons[5]) {
+            new Classe();
+        }
+        
+        if(e.getSource()== lstButtons[6]) {
+            new Disciplina();
         }
     }
 }
